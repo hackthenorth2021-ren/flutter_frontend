55d017f125ffb8deec8b495cb2cce73f4584d03d
Kevin Chen <kevinchen828@hotmail.com>
2021-09-18 11:27:54 -0400

    test camera

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend/components/card.dart';
+import 'package:flutter_frontend/screens/camera.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../components/constants.dart';
 
@@ -46,8 +47,8 @@ class __searchBarState extends State<_searchBar> {
                 ),
                 SizedBox(width: 11),
                 Container(
-                  //margin: EdgeInsets.only(bottom: 5),
-                  width: 300,
+                  //margin: EdgeInsets.only(right: 500),
+                  width: MediaQuery.of(context).size.width - 80,
                   child: TextField(
                     decoration: new InputDecoration(
                       hintText: 'Search Recipe',
@@ -66,8 +67,70 @@ class __searchBarState extends State<_searchBar> {
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class RecipeCard extends StatelessWidget {
+  const RecipeCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      child: ReusableCard(
+        colour: kIsabel,
+        cardChild: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 5 / 2,
+              child: Card(
+                semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                child: Image.asset(
+                  'assets/sadge.jpg',
+                  fit: BoxFit.fitWidth,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 5,
+                margin: EdgeInsets.all(0),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            ListTile(
+              title: Text("Im gay"),
+              subtitle: Text("Kevin has gay benis"),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    Home(),
+    TakePictureScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,7 +142,7 @@ class Home extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
             const ListTile(
-              leading: Icon(Icons.image),
+              //leading: Icon(Icons.image),
               title: Text(
                 "Hello Douglas",
                 style: TextStyle(
@@ -91,41 +154,32 @@ class Home extends StatelessWidget {
                   "Vestibulum quis ligula nec neque ornare feugiat. Nunc condimentum, nunc su"),
             ),
             _searchBar(),
-            ReusableCard(
-                colour: kOpal,
-                cardChild: Column(
-                  children: [
-                    AspectRatio(
-                      aspectRatio: 5 / 2,
-                      child: Card(
-                        semanticContainer: true,
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-                        child: Image.asset(
-                          'assets/sadge.jpg',
-                          fit: BoxFit.fitWidth,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        elevation: 5,
-                        margin: EdgeInsets.all(0),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    ListTile(
-                      title: Text("Im gay"),
-                      subtitle: Text("Kevin has gay benis"),
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                  ],
-                )),
+            Expanded(
+              child: ListView(
+                //shrinkWrap: true,
+                children: [RecipeCard(), RecipeCard(), RecipeCard()],
+              ),
+            )
           ])),
-      //bottomNavigationBar:
-      //    BottomNavigationBar(items: const <BottomNavigationBarItem>[]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.utensils),
+            label: 'Inventory',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.camera),
+            label: 'Add Food',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
